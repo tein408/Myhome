9fd917c41fc205281bf17183bd88e71db4660cde
tein408 <tein408@gmail.com>
2022-01-27 22:32:58 +0900

    feat 게시판 목록 조회 추가

---
+++ src/main/java/com/myhome/service/BoardService.java
@@ -0,0 +1,22 @@
+package com.myhome.service;
+
+import com.myhome.domain.Board;
+import com.myhome.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public List<Board> findBoards() {
+        return boardRepository.findAll();
+    }
+
+}
