e620ce9c2527396038986ed2f51b5eb8395706b7
tein408 <tein408@gmail.com>
2022-01-27 23:15:51 +0900

    feat BoardForm 추가

---
+++ src/main/java/com/myhome/controller/BoardForm.java
@@ -0,0 +1,14 @@
+package com.myhome.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BoardForm {
+
+    private Long id;
+
+    private String title;
+    private String content;
+
+}
