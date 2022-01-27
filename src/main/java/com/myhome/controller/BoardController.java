2226814e7e8774a800ced16e906261b17e4ad5e7
tein408 <tein408@gmail.com>
2022-01-27 22:40:21 +0900

    feat 글 개별 조회 추가

--- src/main/java/com/myhome/controller/BoardController.java
+++ src/main/java/com/myhome/controller/BoardController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -20,6 +21,13 @@ public class BoardController {
         List<Board> boardList = boardService.findBoards();
         model.addAttribute("boardList", boardList);
         return "generic";
+    }
+
+    @GetMapping(value = "/generic/{id}")
+    public String board(@PathVariable ("id") Long id, Model model) {
+        Board board = boardService.findOne(id);
+        model.addAttribute("board", board);
+        return "generic/board";
     }
 
 }
