76029fd855e6707e06d8c16385955e6d26b808c6
tein408 <tein408@gmail.com>
2022-01-28 00:46:21 +0900

    feat: 글 목록 조회가능하도록 추가

--- src/main/java/com/myhome/controller/HomeController.java
+++ src/main/java/com/myhome/controller/HomeController.java
@@ -1,20 +1,31 @@
 package com.myhome.controller;
 
+import com.myhome.domain.Board;
+import com.myhome.service.BoardService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
 public class HomeController {
 
+    private BoardService boardService;
+
     @GetMapping(value = "/")
-    public String home() {
+    public String home(Model model) {
+        List<Board> boardList = boardService.findBoards();
+        model.addAttribute("boardList", boardList);
         return "index";
     }
 
     @GetMapping(value = "/index")
-    public String index() {
+    public String index(Model model) {
+        List<Board> boardList = boardService.findBoards();
+        model.addAttribute("boardList", boardList);
         return "index";
     }
 
