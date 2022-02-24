83954c2e7e95f95fade837e518c16e0729b80b53
tein408 <tein408@gmail.com>
2022-02-25 01:56:22 +0900

    fix: 어노테이션 수정
    - @ColumnDefault 작동 되지 않음
    - Column은 그대로 두고, entity에 @DynamicInsert 추가
    - 값을 입력받지 않아 null이 된 field를 default값으로 변경하는 DDL 수행시 발생하는 에러 해결

--- src/main/java/com/myhome/domain/User.java
+++ src/main/java/com/myhome/domain/User.java
@@ -2,8 +2,8 @@ package com.myhome.domain;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
@@ -11,6 +11,7 @@ import java.sql.Timestamp;
 @Entity
 @Getter
 @Setter
+@DynamicInsert
 public class User {
 
     @Id
@@ -23,7 +24,6 @@ public class User {
     @Column(nullable = false, length = 100)
     private String password;
 
-    @ColumnDefault(value = "USER")
     @Enumerated(EnumType.STRING)
     private Role role;
 
