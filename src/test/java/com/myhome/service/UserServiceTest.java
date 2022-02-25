b2420357a88f406af18e0339bede53be1178b0a5
tein408 <tein408@gmail.com>
2022-02-26 01:19:00 +0900

    test: 회원 전체 조회 테스트

--- src/test/java/com/myhome/service/UserServiceTest.java
+++ src/test/java/com/myhome/service/UserServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @SpringBootTest
 @Transactional
@@ -52,6 +53,34 @@ class UserServiceTest {
         //then
         User changeUser = userRepository.findOne(saveUid);
         Assertions.assertEquals("바꾼비밀번호", changeUser.getPassword());
+    }
+
+    @Test
+    public void 회원전체조회() throws Exception {
+        //given
+        User user = new User();
+        user.setId("아이디");
+        user.setPassword("비밀번호");
+        user.setRole(Role.USER);
+
+        User user1 = new User();
+        user1.setId("아이디1");
+        user1.setPassword("비밀번호1");
+        user1.setRole(Role.USER);
+
+        User user2 = new User();
+        user2.setId("아이디2");
+        user2.setPassword("비밀번호2");
+        user2.setRole(Role.USER);
+
+        //when
+        userService.save(user);
+        userService.save(user1);
+        userService.save(user2);
+
+        //then
+        List<User> userList = userRepository.findAll();
+        Assertions.assertEquals(3, userList.size());
     }
 
 }
